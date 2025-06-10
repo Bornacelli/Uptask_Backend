ca179b05356ae4bd20b61aff2b9443f6b54c776a
Laura Bornacelli <laurabornacelli02@gmail.com>
2025-06-10 16:33:48 -0500

    Ajustes

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,39 @@
-import { CorsOptions } from "cors";
+
+import { CorsOptions } from 'cors';
 
 export const corsConfig: CorsOptions = {
-    origin: function(origin, callback) {
-        console.log(process.argv)
-        const whitelist = [process.env.FRONTEND_URL]   
-    if(process.argv[2] == '--api') {
-        whitelist.push(undefined)
-    }   
-    
-    if (whitelist.includes(origin)) {
+    origin: function (origin, callback) {
+        const allowedOrigins = [
+            'https://up-task-frontend-nine-chi.vercel.app',
+            'http://localhost:3000',
+            'http://localhost:5173',
+            'http://localhost:4173',
+            undefined // Para requests sin origin
+        ];
+
+        if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
             callback(new Error('No permitido por CORS'));
         }
-    }
-}
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: [
+        'Content-Type', 
+        'Authorization', 
+        'X-Requested-With',
+        'Accept',
+        'Origin'
+    ],
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+};
+
+// Para testing - permite cualquier origen
+export const corsConfigOpen: CorsOptions = {
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin']
+};
